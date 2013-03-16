2623fe5b490445b762cb96a0795fa9232c8998ad
josepe <jmhurtado@gmail.com>
2013-03-16 01:48:44 -0400

    resize methods added to Band

---
+++ +hp/@Band/clone_reshape.m
@@ -0,0 +1,12 @@
+         function  out=clone_reshape(in,winlength)
+         %winlength in seconds
+            import hp.*
+            out=in;
+            len=length(out.data);
+            samplesperwin=floor(winlength*out.sampleFreq);
+            nwins=floor(len/samplesperwin);
+            usedata=in.data(1:nwins*samplesperwin);
+            usetime=in.tvector(1:nwins*samplesperwin);
+            out.data=reshape(usedata,samplesperwin,nwins);
+            out.tvector=reshape(usetime,samplesperwin,nwins);
+          end
